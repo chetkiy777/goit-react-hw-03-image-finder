3ee6b05e505f4705c4de0ed160c6f6ac97eb1f88
Igor Gladkiy <egunka1988@gmail.com>
2022-02-15 15:54:53 +0200

    install axios, create folder and files template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,3 @@
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div>pixabay</div>;
 };
